57b12e013c3ad9f9d270e8e0992f2e2939bc3bc0
Sandra To <sandra.to.e@gmail.com>
2021-04-01 02:43:05 +0000

    Add basic authentication to admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  # Basic Admin Authenticcation
+  http_basic_authenticate_with name: ENV['ADMIN_BASIC_AUTH_USERNAME'], password: ENV['ADMIN_BASIC_AUTH_PASSWORD']
+  
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  # Basic Admin Authenticcation
+  http_basic_authenticate_with name: ENV['ADMIN_BASIC_AUTH_USERNAME'], password: ENV['ADMIN_BASIC_AUTH_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
