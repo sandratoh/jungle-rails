3e381eae9b43b9f8330f7d31a035acfdcc888d94
Sandra To <sandra.to.e@gmail.com>
2021-04-02 08:44:30 +0000

    Revert back to redirect instead of rrender due to unfound users view error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      render :new
+      redirect_to '/register'
     end
   end
 
