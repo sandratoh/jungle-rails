2ff6bc7d88da5057195300bcd7ec9a79d378180c
Sandra To <sandra.to.e@gmail.com>
2021-04-02 00:58:14 +0000

    Add index and new method and link app to correct views

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,9 +3,11 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_BASIC_AUTH_USERNAME'], password: ENV['ADMIN_BASIC_AUTH_PASSWORD']
 
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
