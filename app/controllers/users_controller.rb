a86f8e66af07624b0c6c03f931b91152601cb2a7
Sandra To <sandra.to.e@gmail.com>
2021-04-02 08:17:35 +0000

    Add class validation when register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
@@ -9,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/register'
+      render :new
     end
   end
 
