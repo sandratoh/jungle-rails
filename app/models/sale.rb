80334ddd63fd846d87ab8fd27d815c638d32ab4c
Sandra To <sandra.to.e@gmail.com>
2021-04-02 10:53:35 +0000

    Add custom and built-in validations to sale class

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,21 @@
 class Sale < ActiveRecord::Base
 
+  validates :name, presence: true
+  validates :percent_off, presence: true, 
+                          numericality: { greater_than: 0 }
+  validates :starts_on, presence: true
+  validates :ends_on, presence: true
+  validate :end_date_must_be_later_than_start_date
+
+  # Custom Validation
+  def end_date_must_be_later_than_start_date
+    unless ends_on.nil? && starts_on.nil?
+      if ends_on < starts_on
+        errors.add(:ends_on, "must be later than Starts on")
+      end
+    end
+  end
+
   # AR Scope
   def self.active
     where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
