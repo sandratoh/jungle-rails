14f4c27b56198dee6127ec95c34613a8dc3a5b2d
Sandra To <sandra.to.e@gmail.com>
2021-04-06 02:14:44 +0000

    Add first/last name user auth and additional email validation when logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,17 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :name, presence: true
+  validates :first_name, :last_name, presence: true
   validates :email, { presence: true, uniqueness: { case_sensitive: false } }
   validates :password, length: { minimum: 4 }
 
   def self.authenticate_with_credentials(email, password)
-    has_secure_password ? true : false
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,42 @@
-require 'rails_helper'
+# require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
 
-    context 'given name, email, password, and matching password_confirmation input fields' do
+    context 'given first name, last name, email, password, and matching password_confirmation input fields' do
       it 'should save successfully' do
-        @user = User.new(name: 'Michael', email: 'michael@test.com', password: 'test', password_confirmation: 'test')
+        @user = User.new(first_name: 'Michael', last_name: 'Scott', email: 'michael@test.com', password: 'test', password_confirmation: 'test')
         expect(@user).to be_valid
       end
     end
 
-    context 'given missing name, email, password, or matching password input fields' do
-      it 'should require name' do
-        @user = User.new(email: 'michael@test.com', password: 'test', password_confirmation: 'test')
+    context 'given missing first name, last name, email, password, or matching password input fields' do
+      it 'should require first name' do
+        @user = User.new(last_name: 'Scott', email: 'michael@test.com', last_name: 'Scott', password: 'test', password_confirmation: 'test')
         expect(@user).not_to be_valid
-        expect(@user.errors.full_messages).to include("Name can't be blank")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+
+      it 'should require last name' do
+        @user = User.new(first_name: 'Michael', email: 'michael@test.com', password: 'test', password_confirmation: 'test')
+        expect(@user).not_to be_valid
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
 
       it 'should require email' do
-        @user = User.new(name: 'Michael', password: 'test', password_confirmation: 'test')
+        @user = User.new(first_name: 'Michael', last_name: 'Scott', password: 'test', password_confirmation: 'test')
         expect(@user).not_to be_valid
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
 
       it 'should require password' do
-        @user = User.new(name: 'Michael', email: 'michael@test.com')
+        @user = User.new(first_name: 'Michael', last_name: 'Scott', email: 'michael@test.com')
         expect(@user).not_to be_valid
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
       it 'should require matching password confirmation' do
-        @user = User.new(name: 'Michael', email: 'michael@test.com', password: 'test', password_confirmation: 'wrongpassword')
+        @user = User.new(first_name: 'Michael', last_name: 'Scott', email: 'michael@test.com', password: 'test', password_confirmation: 'wrongpassword')
         expect(@user).not_to be_valid
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
@@ -38,17 +44,17 @@ RSpec.describe User, type: :model do
 
     context 'user email input' do
       before do
-        @user = User.create(name: 'Michael', email: 'michael@test.com', password: 'test', password_confirmation: 'test')
+        @user = User.create(first_name: 'Michael', last_name: 'Scott', email: 'michael@test.com', password: 'test', password_confirmation: 'test')
       end
 
       it 'should not save if exact email already exists in database' do
-        @new_user = User.new(name: 'Michael', email: 'michael@test.com', password: 'test', password_confirmation: 'test')
+        @new_user = User.new(first_name: 'Michael', last_name: 'Scott', email: 'michael@test.com', password: 'test', password_confirmation: 'test')
         expect(@new_user).not_to be_valid
         expect(@new_user.errors.full_messages).to include("Email has already been taken")
       end
 
       it 'should be unique and not case sensitive' do
-        @new_user = User.new(name: 'Michael', email: 'michael@test.COM', password: 'test', password_confirmation: 'test')
+        @new_user = User.new(first_name: 'Michael', last_name: 'Scott', email: 'michael@test.COM', password: 'test', password_confirmation: 'test')
         expect(@new_user).not_to be_valid
         expect(@new_user.errors.full_messages).to include("Email has already been taken")
       end
@@ -56,7 +62,7 @@ RSpec.describe User, type: :model do
 
     context 'password input' do
       it 'should have a minimum length of 4' do
-        @user = User.new(name: 'Michael', email: 'michael@test.COM', password: 'abc', password_confirmation: 'abc')
+        @user = User.new(first_name: 'Michael', last_name: 'Scott', email: 'michael@test.COM', password: 'abc', password_confirmation: 'abc')
         expect(@user).not_to be_valid
       end
     end
@@ -65,7 +71,7 @@ RSpec.describe User, type: :model do
 
   describe '.authenticate_with_credentials' do
     before do
-      User.create(name: 'Michael', email: 'michael@dundermifflin.com', password: 'test', password_confirmation: 'test')
+      @user = User.create(first_name: 'Michael', last_name: 'Scott', email: 'michael@dundermifflin.com', password: 'test', password_confirmation: 'test')
       @email = 'michael@dundermifflin.com'
       @password = 'test'
     end
@@ -74,6 +80,7 @@ RSpec.describe User, type: :model do
       it 'should log user in' do
         @user_login = User.authenticate_with_credentials(@email, @password)
         expect(@user_login).not_to be_nil
+        expect(@user_login.email).to eql(@user.email)
       end
     end
 
@@ -98,6 +105,7 @@ RSpec.describe User, type: :model do
         @email = 'MICHAEL@dundermifflin.com'
         @user_login = User.authenticate_with_credentials(@email, @password)
         expect(@user_login).not_to be_nil
+        expect(@user_login.email).to eql(@user.email)
       end
     end
 
@@ -106,12 +114,14 @@ RSpec.describe User, type: :model do
         @email = '  michael@dundermifflin.com'
         @user_login = User.authenticate_with_credentials(@email, @password)
         expect(@user_login).not_to be_nil
+        expect(@user_login.email).to eql(@user.email)
       end
 
       it 'should log user in with whitespaces after email' do
         @email = 'michael@dundermifflin.com  '
         @user_login = User.authenticate_with_credentials(@email, @password)
         expect(@user_login).not_to be_nil
+        expect(@user_login.email).to eql(@user.email)
       end
     end
 
