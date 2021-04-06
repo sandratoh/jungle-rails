026f0f3570cf7890679b3a3830dc3b7b96d96b79
Sandra To <sandra.to.e@gmail.com>
2021-04-06 01:45:14 +0000

    Remove name column from user db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210406013757) do
+ActiveRecord::Schema.define(version: 20210406014353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 20210406013757) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
