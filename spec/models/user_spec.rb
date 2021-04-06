77698f0a4971b2bf2d2bc0fca9ebda4e13929131
Sandra To <sandra.to.e@gmail.com>
2021-04-06 01:17:08 +0000

    Refactor user auth specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -65,53 +65,49 @@ RSpec.describe User, type: :model do
 
   describe '.authenticate_with_credentials' do
     before do
-      User.create(name: 'Michael', email: 'michael@test.com', password: 'test', password_confirmation: 'test')
+      User.create(name: 'Michael', email: 'michael@dundermifflin.com', password: 'test', password_confirmation: 'test')
+      @email = 'michael@dundermifflin.com'
+      @password = 'test'
     end
 
     context 'with correct authentication input' do
       it 'should log user in' do
-        email = 'michael@test.com'
-        password = 'test'
-        @user_login = User.authenticate_with_credentials(email, password)
+        @user_login = User.authenticate_with_credentials(@email, @password)
         expect(@user_login).not_to be_nil
       end
     end
 
     context 'with nonexisting email' do
       it 'should not log user in' do
-        email = 'michael@wrongemail.com'
-        password = 'test'
-        @user_login = User.authenticate_with_credentials(email, password)
+        @email = 'michael@wrongemail.com'
+        @user_login = User.authenticate_with_credentials(@email, @password)
         expect(@user_login).to be_falsey
       end
     end
 
     context 'with incorrect password' do
       it 'should not log user in' do
-       email = 'michael@test.com'
-       password = 'TEST'
-       @user_login = User.authenticate_with_credentials(email, password)
+       @password = 'TEST'
+       @user_login = User.authenticate_with_credentials(@email, @password)
        expect(@user_login).to be_falsey
       end
     end
 
     context 'with non-case-sensitive email' do
       it 'should log user in' do
-        email = 'MICHAEL@test.com'
-        password = 'test'
-        @user_login = User.authenticate_with_credentials(email, password)
+        @email = 'MICHAEL@dundermifflin.com'
+        @user_login = User.authenticate_with_credentials(@email, @password)
         expect(@user_login).not_to be_nil
       end
     end
 
     context 'with whitespaces before email' do
       it 'should log user in' do
-        email = '  michael@test.com'
-        password = 'test'
-        @user_login = User.authenticate_with_credentials(email, password)
+        @email = '  michael@dundermifflin.com'
+        @user_login = User.authenticate_with_credentials(@email, @password)
         expect(@user_login).not_to be_nil
       end
     end
-    
+
   end
 end
