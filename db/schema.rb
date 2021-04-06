decedbd1216e788cb9e692c48f1e79af89ec1581
Sandra To <sandra.to.e@gmail.com>
2021-04-06 01:43:06 +0000

    Add first name last name to user db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210402091322) do
+ActiveRecord::Schema.define(version: 20210406013757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20210402091322) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_foreign_key "line_items", "orders"
