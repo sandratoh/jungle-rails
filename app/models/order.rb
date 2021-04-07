94ae6a7d59b8259b2993a467939a693855858cfb
Sandra To <sandra.to.e@gmail.com>
2021-04-07 04:58:21 +0000

    Implement item quantity change after successful order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,15 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_create :reduce_item_quantity
+
+  private
+
+  def reduce_item_quantity
+    self.line_items.each do |item|
+      item.product.quantity -= item.quantity
+      item.product.save!
+    end
+  end
+    
 end
